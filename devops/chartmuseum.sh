43af323a212d13211506d8c262f8055c68a55c62
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2021-10-11 22:53:33 -0300

    installing chartmuseum
    helm install --name helm --namespace devops -f chartmuseum-conf.yaml stable/chartmuseum
    
    helm plugin install https://github.com/chartmuseum/helm-push
    
    helm repo add questcode http://$(kubectl get nodes --namespace devops -o jsonpath="{.items[0].status.addresses[0].address}"):30010

---
+++ devops/chartmuseum.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+helm lint charts/backend-scm/
+helm push charts/backend-scm/ questcode
+
+helm lint charts/backend-user/
+helm push charts/backend-user/ questcode
+
+helm lint charts/frontend/
+helm push charts/frontend/ questcode
+
+helm repo update
